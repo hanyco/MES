9ee03c366e6bdd443e2fbe8cafade6bea4f8e383
Mohammad Mirmostafa <mirmostafa@hotmail.com>
2025-06-12 18:58:27 +0330

    Refactor EnumerableExtension and update dependencies
    - Removed `Library.Helpers` and added `Microsoft.CodeAnalysis.CSharp` in `GeneratorHelper.cs`.
    - Enhanced XML documentation for `AddRange` and updated its implementation to check for null items.
    - Improved clarity of `EmptyArray` documentation.
    - Introduced `SelectAll` method for selecting elements from a sequence of sequences.
    - Modified `Except` method to check for null predicates.
    - Added `ContainsKey` and `GetValueByKey` methods for tuple collections.
    - Overall improvements to the structure and documentation of the `EnumerableExtension` class.
    
    تغییرات شامل به‌روزرسانی وابستگی‌ها و بهبود مستندات XML برای متدهای مختلف است. همچنین متدهای جدیدی برای افزایش کارایی کلاس `EnumerableExtension` اضافه شده است.

--- src/bcl/CoreLib/Extensions/EnumerableExtension.cs
+++ src/bcl/CoreLib/Extensions/EnumerableExtension.cs
@@ -1,19 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Immutable;
-using System.Diagnostics.CodeAnalysis;
-
-using Library.Extensions;
-using Library.Validations;
 
 namespace Library.Extensions;
 
 public static class EnumerableExtension
 {
     /// <summary>
+    /// Adds a range of items to the specified collection.
+    /// </summary>
+    /// <typeparam name="TList"> The type of the collection to which items will be added. </typeparam>
+    /// <typeparam name="TItem"> The type of the items to be added. </typeparam>
+    /// <param name="list">  The collection to which the items will be added. </param>
+    /// <param name="items"> The items to be added to the collection. </param>
+    /// <returns> The updated collection with added items. </returns>
+    /// <remarks>
+    /// This extension method allows adding a range of items to a collection that implements
+    /// ICollection. The method checks if the 'items' enumerable is not null and contains items
+    /// before performing the addition.
+    /// </remarks>
+    public static TList AddRange<TList, TItem>([DisallowNull] this TList list, in IEnumerable<TItem> items)
+        where TList : ICollection<TItem>
+    {
+        if (items?.Any() is true)
+        {
+            // Iterate through each item in the 'items' enumerable and add it to the collection.
+            foreach (var item in items)
+            {
+                list.Add(item);
+            }
+        }
+        return list; // Return the updated collection with added items.
+    }
+
+    public static IEnumerable<T> AsEnumerable<T>(T item)
+    {
+        yield return item;
+    }
+
+    /// <summary>
     /// Creates an empty array of the specified type.
     /// </summary>
-    /// <typeparam name="T">The type of the array elements.</typeparam>
-    /// <returns>An empty array of type T.</returns>
+    /// <typeparam name="T"> The type of the array elements. </typeparam>
+    /// <returns> An empty array of type T. </returns>
     public static T[] EmptyArray<T>()
         => [];
 
@@ -71,13 +99,38 @@ public static class EnumerableExtension
             }
             yield return item;
         }
+
+        /// <summary>
+        /// Returns a collection of elements from the input sequence that do not satisfy the
+        /// specified predicate.
+        /// </summary>
+        public IEnumerable<TItem> Except(Func<TItem, bool> exceptor)
+        {
+            Check.MustBeArgumentNotNull(exceptor);
+
+            return source.Where(x => !exceptor(x));
+        }
+
+        public TResult? SelectImmutable<TResult>(in Func<TItem?, TResult?, TResult?> selector, in TResult? defaultResult = default)
+        {
+            var result = defaultResult;
+            if (source is { } && source.Any())
+            {
+                Check.MustBeArgumentNotNull(selector);
+                foreach (var item in source)
+                {
+                    result = selector(item, result);
+                }
+            }
+            return result;
+        }
     }
 
     /// <summary>
     /// Selects all elements from a sequence of sequences.
     /// </summary>
-    /// <param name="values">The sequence of sequences.</param>
-    /// <returns>A sequence containing all elements of the input sequences.</returns>
+    /// <param name="values"> The sequence of sequences. </param>
+    /// <returns> A sequence containing all elements of the input sequences. </returns>
     public static IEnumerable<T> SelectAll<T>(this IEnumerable<IEnumerable<T>> values)
     {
         Check.MustBeArgumentNotNull(values);
@@ -91,76 +144,23 @@ public static class EnumerableExtension
         }
     }
 
-    public static IEnumerable<T> AsEnumerable<T>(T item)
+    extension<TKey, TValue>(IEnumerable<(TKey Key, TValue Value)> source)
     {
-        yield return item;
+        /// <summary>
+        /// Checks if the given IEnumerable contains a key-value pair with the specified key.
+        /// </summary>
+        public bool ContainsKey(TKey key) =>
+            source.ArgumentNotNull().Any(kv => kv.Key?.Equals(key) ?? key is null);
+
+        /// <summary>
+        /// Gets the value from the given source by the specified key.
+        /// </summary>
+        /// <typeparam name="TKey"> The type of the key. </typeparam>
+        /// <typeparam name="TValue"> The type of the value. </typeparam>
+        /// <param name="source"> The source. </param>
+        /// <param name="key">    The key. </param>
+        /// <returns> The value. </returns>
+        public TValue GetValueByKey(TKey key) =>
+            source.ArgumentNotNull().First(kv => kv.Key?.Equals(key) ?? key is null).Value;
     }
-
-    /// <summary>
-    /// Adds a range of items to the specified collection.
-    /// </summary>
-    /// <typeparam name="TList">The type of the collection to which items will be added.</typeparam>
-    /// <typeparam name="TItem">The type of the items to be added.</typeparam>
-    /// <param name="list">The collection to which the items will be added.</param>
-    /// <param name="items">The items to be added to the collection.</param>
-    /// <returns>The updated collection with added items.</returns>
-    /// <remarks>
-    /// This extension method allows adding a range of items to a collection that implements
-    /// ICollection. The method checks if the 'items' enumerable is not null and contains items
-    /// before performing the addition.
-    /// </remarks>
-    public static TList AddRange<TList, TItem>([DisallowNull] this TList list, in IEnumerable<TItem> items)
-        where TList : ICollection<TItem>
-    {
-        if (items?.Any() is true)
-        {
-            // Iterate through each item in the 'items' enumerable and add it to the collection.
-            foreach (var item in items)
-            {
-                list.Add(item);
-            }
-        }
-        return list; // Return the updated collection with added items.
-    }
-
-    /// <summary>
-    /// Checks if the given IEnumerable contains a key-value pair with the specified key.
-    /// </summary>
-    public static bool ContainsKey<TKey, TValue>([DisallowNull] this IEnumerable<(TKey Key, TValue Value)> source, TKey key)
-        => source.ArgumentNotNull().Where(kv => kv.Key?.Equals(key) ?? key is null).Any();
-
-    /// <summary>
-    /// Gets the value from the given source by the specified key.
-    /// </summary>
-    /// <typeparam name="TKey">The type of the key.</typeparam>
-    /// <typeparam name="TValue">The type of the value.</typeparam>
-    /// <param name="source">The source.</param>
-    /// <param name="key">The key.</param>
-    /// <returns>The value.</returns>
-    public static TValue GetValueByKey<TKey, TValue>(this IEnumerable<(TKey Key, TValue Value)> source, TKey key) =>
-        source.ArgumentNotNull().First(kv => kv.Key?.Equals(key) ?? key is null).Value;
-
-    public static TResult? SelectImmutable<TItem, TResult>(this IEnumerable<TItem?> items, in Func<TItem?, TResult?, TResult?> selector, in TResult? defaultResult = default)
-    {
-        var result = defaultResult;
-        if (items is { } && items.Any())
-        {
-            Check.MustBeArgumentNotNull(selector);
-            foreach (var item in items)
-            {
-                result = selector(item, result);
-            }
-        }
-        return result;
-    }
-    /// <summary>
-    /// Returns a collection of elements from the input sequence that do not satisfy the specified predicate.
-    /// </summary>
-    public static IEnumerable<T> Except<T>(this IEnumerable<T> items, Func<T, bool> exceptor)
-    {
-        Check.MustBeArgumentNotNull(exceptor);
-
-        return items.Where(x => !exceptor(x));
-    }
-
 }
