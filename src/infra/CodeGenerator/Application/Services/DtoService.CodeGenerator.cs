b1ae847ace723486b7200c35e8db11aea902aa83
Mohammad Mirmostafa <mirmostafa@hotmail.com>
2025-06-14 08:56:12 -0700

    [CodeGenerator] remove CQRS and integrate Roslyn generator

---
+++ src/infra/CodeGenerator/Application/Services/DtoService.CodeGenerator.cs
@@ -0,0 +1,28 @@
+using CodeGenerator.Application.Domain;
+using Library.CodeGenLib.Back;
+using Library.CodeGenLib.CodeGenerators;
+using Library.CodeGenLib.Models;
+using Library.Coding;
+
+namespace CodeGenerator.Application.Services;
+
+public partial class DtoService
+{
+    /// <summary>
+    /// تولید کد کلاس DTO به صورت partial.
+    /// </summary>
+    public Code GenerateCode(DtoDefinition dto)
+    {
+        var ns = INamespace.New(dto.Namespace);
+        var cls = IClass.New(dto.Name);
+        foreach (var field in dto.Fields)
+        {
+            var prop = IProperty.New(field.Name, TypePath.New(field.Type));
+            cls.AddProperty(prop);
+        }
+        ns.AddType(cls);
+
+        var codeResult = ns.GenerateCode<RoslynCodeGenerator>();
+        return new Code(dto.Name, Languages.CSharp, codeResult.Value!, isPartial: true);
+    }
+}
