9a257b76cd4f915aa9dc14436186d0fd0192430c
Mohammad Mirmostafa <mirmostafa@hotmail.com>
2025-06-27 22:46:23 +0330

    Refactor App and MainWindow for improved structure
    Updated App.xaml to fix closing tag. Revamped MainWindow.xaml with a new menu structure for better organization, adjusting window dimensions accordingly. Modified MainWindow.xaml.cs to reflect new menu item handlers and removed outdated methods.
    
    این تغییرات شامل اصلاحات در ساختار و سازماندهی منوها و به‌روزرسانی متدهای مربوط به آن‌ها است.

--- src/infra/CodeGenerator/MainWindow.xaml.cs
+++ src/infra/CodeGenerator/MainWindow.xaml.cs
@@ -1,4 +1,4 @@
-using System.Windows;
+﻿using System.Windows;
 
 namespace CodeGenerator;
 /// <summary>
@@ -11,18 +11,38 @@ public partial class MainWindow : Window
         InitializeComponent();
     }
 
-    private void OnManageDto(object sender, RoutedEventArgs e)
+    private void ManageModels_Click(object sender, RoutedEventArgs e)
     {
-        MessageBox.Show("مدیریت DTO انتخاب شد");
+        MessageBox.Show("Manage Models selected", "Info");
     }
 
-    private void OnManageCqrs(object sender, RoutedEventArgs e)
+    private void ManageCqrsQueries_Click(object sender, RoutedEventArgs e)
     {
-        MessageBox.Show("مدیریت CQRS انتخاب شد");
+        MessageBox.Show("Manage CQRS Queries selected", "Info");
     }
 
-    private void OnSettings(object sender, RoutedEventArgs e)
+    private void ManageCqrsCommands_Click(object sender, RoutedEventArgs e)
     {
-        MessageBox.Show("تنظیمات انتخاب شد");
+        MessageBox.Show("Manage CQRS Commands selected", "Info");
+    }
+
+    private void BlazorComponents_Click(object sender, RoutedEventArgs e)
+    {
+        MessageBox.Show("Blazor Components selected", "Info");
+    }
+
+    private void BlazorPages_Click(object sender, RoutedEventArgs e)
+    {
+        MessageBox.Show("Blazor Pages selected", "Info");
+    }
+
+    private void Settings_Click(object sender, RoutedEventArgs e)
+    {
+        MessageBox.Show("Settings selected", "Info");
+    }
+
+    private void About_Click(object sender, RoutedEventArgs e)
+    {
+        MessageBox.Show("About selected", "Info");
     }
 }
