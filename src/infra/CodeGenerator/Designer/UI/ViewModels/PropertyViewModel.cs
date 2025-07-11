be2ccaa70dcf21a7766296ae85fc6c5af2530a06
Mohammad Mirmostafa <mirmostafa@hotmail.com>
2025-07-12 00:00:28 +0330

    Code cleanup

--- src/infra/CodeGenerator/Designer/UI/ViewModels/PropertyViewModel.cs
+++ src/infra/CodeGenerator/Designer/UI/ViewModels/PropertyViewModel.cs
@@ -1,9 +1,78 @@
-namespace CodeGenerator.Designer.UI.ViewModels;
+﻿namespace CodeGenerator.Designer.UI.ViewModels;
 
 public partial class PropertyViewModel : ViewModelBase
 {
+    public string? Comment
+    {
+        get;
+        set
+        {
+            field = value;
+            this.OnPropertyChanged();
+        }
+    }
 
-    public long Id
+    public string? DbObjectId
+    {
+        get;
+        set
+        {
+            field = value;
+            this.OnPropertyChanged();
+        }
+    }
+
+    public long? DtoId
+    {
+        get;
+        set
+        {
+            field = value;
+            this.OnPropertyChanged();
+        }
+    }
+
+    public Guid Guid
+    {
+        get;
+        set
+        {
+            field = value;
+            this.OnPropertyChanged();
+        }
+    }
+
+    public bool? HasGetter
+    {
+        get;
+        set
+        {
+            field = value;
+            this.OnPropertyChanged();
+        }
+    }
+
+    public bool? HasSetter
+    {
+        get;
+        set
+        {
+            field = value;
+            this.OnPropertyChanged();
+        }
+    }
+
+    public bool? IsList
+    {
+        get;
+        set
+        {
+            field = value;
+            this.OnPropertyChanged();
+        }
+    }
+
+    public bool? IsNullable
     {
         get;
         set
@@ -34,96 +103,6 @@ public partial class PropertyViewModel : ViewModelBase
     }
 
     public string? TypeFullName
-    {
-        get;
-        set
-        {
-            field = value;
-            this.OnPropertyChanged();
-        }
-    }
-
-    public string Name
-    {
-        get;
-        set
-        {
-            field = value;
-            this.OnPropertyChanged();
-        }
-    } = string.Empty;
-
-    public bool? HasSetter
-    {
-        get;
-        set
-        {
-            field = value;
-            this.OnPropertyChanged();
-        }
-    }
-
-    public bool? HasGetter
-    {
-        get;
-        set
-        {
-            field = value;
-            this.OnPropertyChanged();
-        }
-    }
-
-    public bool? IsList
-    {
-        get;
-        set
-        {
-            field = value;
-            this.OnPropertyChanged();
-        }
-    }
-
-    public bool? IsNullable
-    {
-        get;
-        set
-        {
-            field = value;
-            this.OnPropertyChanged();
-        }
-    }
-
-    public string? Comment
-    {
-        get;
-        set
-        {
-            field = value;
-            this.OnPropertyChanged();
-        }
-    }
-
-    public string? DbObjectId
-    {
-        get;
-        set
-        {
-            field = value;
-            this.OnPropertyChanged();
-        }
-    }
-
-    public Guid Guid
-    {
-        get;
-        set
-        {
-            field = value;
-            this.OnPropertyChanged();
-        }
-    }
-
-    public long? DtoId
     {
         get;
         set
