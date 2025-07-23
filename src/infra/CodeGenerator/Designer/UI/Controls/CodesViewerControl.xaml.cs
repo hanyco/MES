4c4b6acb2a71a0d2159336ae1acf4a7795260ef8
Mohammad Mirmostafa <mirmostafa@hotmail.com>
2025-07-23 13:14:51 -0700

    [CodeGenerator] rename control and add dynamic code viewer

---
+++ src/infra/CodeGenerator/Designer/UI/Controls/CodesViewerControl.xaml.cs
@@ -0,0 +1,61 @@
+using System.Windows;
+using System.Windows.Controls;
+
+using Library.CodeGenLib.Models;
+
+namespace CodeGenerator.Designer.UI.Controls;
+
+public partial class CodesViewerControl : UserControl
+{
+    public CodesViewerControl() => this.InitializeComponent();
+
+    public Codes Codes
+    {
+        get => (Codes)this.GetValue(CodesProperty);
+        set => this.SetValue(CodesProperty, value);
+    }
+
+    public static readonly DependencyProperty CodesProperty =
+        DependencyProperty.Register(
+            nameof(Codes),
+            typeof(Codes),
+            typeof(CodesViewerControl),
+            new PropertyMetadata(Codes.Empty, OnCodesPropertyChanged));
+
+    private static void OnCodesPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        if (d is CodesViewerControl control && e.NewValue is Codes codes)
+        {
+            control.UpdateTabs(codes);
+        }
+    }
+
+    private void UpdateTabs(Codes codes)
+    {
+        this.Tabs.Items.Clear();
+        foreach (var code in codes)
+        {
+            if (code is null)
+            {
+                continue;
+            }
+
+            var box = new TextBox
+            {
+                Text = code.Statement,
+                AcceptsReturn = true,
+                VerticalScrollBarVisibility = ScrollBarVisibility.Auto,
+                IsReadOnly = true
+            };
+            var tab = new TabItem
+            {
+                Header = code.Name,
+                Content = box
+            };
+            this.Tabs.Items.Add(tab);
+        }
+        this.OnCodesChanged(codes);
+    }
+
+    partial void OnCodesChanged(Codes codes);
+}
