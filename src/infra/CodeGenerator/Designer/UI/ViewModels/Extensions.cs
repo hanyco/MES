d3c39080baf1d47fe8b57a997a9b13c800780fd7
Mohammad Mirmostafa <mirmostafa@hotmail.com>
2025-07-24 06:39:34 -0700

    [CodeGenerator] convert SQL types to .NET types before code generation

--- src/infra/CodeGenerator/Designer/UI/ViewModels/Extensions.cs
+++ src/infra/CodeGenerator/Designer/UI/ViewModels/Extensions.cs
@@ -1,3 +1,4 @@
+using DataLib.SqlServer;
 ﻿namespace CodeGenerator.Designer.UI.ViewModels;
 
 public static class Extensions
@@ -16,7 +17,22 @@ public static class Extensions
             IsViewModel = @this.IsViewModel,
             DbObjectId = @this.ObjectId.ToString(),
             ModuleId = @this.Module?.Id,
-            Properties = [.. @this.Properties],
+            Properties = [.. @this.Properties.Select(p => new Property
+            {
+                Comment = p.Comment,
+                DbObjectId = p.DbObjectId,
+                DtoId = p.DtoId,
+                Guid = p.Guid,
+                HasGetter = p.HasGetter,
+                HasSetter = p.HasSetter,
+                Id = p.Id,
+                IsList = p.IsList,
+                IsNullable = p.IsNullable,
+                Name = p.Name,
+                ParentEntityId = p.ParentEntityId,
+                PropertyType = p.PropertyType,
+                TypeFullName = SqlTypeUtils.ToNetTypeName(p.TypeFullName ?? string.Empty),
+            })],
         };
     }
 }
