18e5607aa60015ffab9be57fb2b45021cbf297eb
Mohammad Mirmostafa <mirmostafa@hotmail.com>
2025-08-08 11:35:21 -0700

    [CodeGenerator] مدیریت instance موقت در SettingsControl

--- src/infra/CodeGenerator/Designer/UI/Pages/SettingsControl.xaml.cs
+++ src/infra/CodeGenerator/Designer/UI/Pages/SettingsControl.xaml.cs
@@ -1,12 +1,15 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
 
+using CodeGenerator.Application.Services;
 using CodeGenerator.UI.Dialogs;
 
 namespace CodeGenerator.Designer.UI.Controls;
 
 public partial class SettingsControl : UserControl
 {
+    private Settings _tempSettings = new() { Folders = new FolderStructure() };
+
     public SettingsControl()
     {
         this.InitializeComponent();
@@ -17,34 +20,38 @@ public partial class SettingsControl : UserControl
 
     private void OnSave(object sender, RoutedEventArgs e)
     {
-        var connectionString = this.ConnectionStringBox.Text;
-        var folders = new FolderStructure
-        {
-            DefaultRoot = this.RootPathBox.Text,
-            PagesPath = this.PagesPathBox.Text,
-            ComponentsPath = this.ComponentsPathBox.Text,
-            ViewModelsPath = this.ViewModelsPathBox.Text,
-            ControllersPath = this.ControllersPathBox.Text,
-            ApplicationPath = this.ApplicationPathBox.Text,
-            ApplicationDtosPath = this.DtosPathBox.Text,
-            RepositoriesPath = this.RepositoriesPathBox.Text
-        };
-        Settings.Configure(connectionString, folders);
+        Settings.Configure(this._tempSettings);
         Settings.Save();
     }
 
     private void LoadFromSettings()
     {
         Settings.Load();
-        this.ConnectionStringBox.Text = Settings.Default.ConnectionString;
-        this.RootPathBox.Text = Settings.Default.Folders?.DefaultRoot;
-        this.PagesPathBox.Text = Settings.Default.Folders?.PagesPath;
-        this.ComponentsPathBox.Text = Settings.Default.Folders?.ComponentsPath;
-        this.ViewModelsPathBox.Text = Settings.Default.Folders?.ViewModelsPath;
-        this.ControllersPathBox.Text = Settings.Default.Folders?.ControllersPath;
-        this.ApplicationPathBox.Text = Settings.Default.Folders?.ApplicationPath;
-        this.DtosPathBox.Text = Settings.Default.Folders?.ApplicationDtosPath;
-        this.RepositoriesPathBox.Text = Settings.Default.Folders?.RepositoriesPath;
+        this._tempSettings = new Settings
+        {
+            ConnectionString = Settings.Default.ConnectionString,
+            Folders = new FolderStructure
+            {
+                DefaultRoot = Settings.Default.Folders?.DefaultRoot,
+                PagesPath = Settings.Default.Folders?.PagesPath,
+                ComponentsPath = Settings.Default.Folders?.ComponentsPath,
+                ViewModelsPath = Settings.Default.Folders?.ViewModelsPath,
+                ControllersPath = Settings.Default.Folders?.ControllersPath,
+                ApplicationPath = Settings.Default.Folders?.ApplicationPath,
+                ApplicationDtosPath = Settings.Default.Folders?.ApplicationDtosPath,
+                RepositoriesPath = Settings.Default.Folders?.RepositoriesPath
+            }
+        };
+
+        this.ConnectionStringBox.Text = this._tempSettings.ConnectionString;
+        this.RootPathBox.Text = this._tempSettings.Folders?.DefaultRoot;
+        this.PagesPathBox.Text = this._tempSettings.Folders?.PagesPath;
+        this.ComponentsPathBox.Text = this._tempSettings.Folders?.ComponentsPath;
+        this.ViewModelsPathBox.Text = this._tempSettings.Folders?.ViewModelsPath;
+        this.ControllersPathBox.Text = this._tempSettings.Folders?.ControllersPath;
+        this.ApplicationPathBox.Text = this._tempSettings.Folders?.ApplicationPath;
+        this.DtosPathBox.Text = this._tempSettings.Folders?.ApplicationDtosPath;
+        this.RepositoriesPathBox.Text = this._tempSettings.Folders?.RepositoriesPath;
     }
 
     private void OnBrowseRootPath(object sender, RoutedEventArgs e)
@@ -118,6 +125,33 @@ public partial class SettingsControl : UserControl
             this.RepositoriesPathBox.Text = path;
         }
     }
+
+    private void OnConnectionStringChanged(object sender, TextChangedEventArgs e)
+        => this._tempSettings.ConnectionString = this.ConnectionStringBox.Text;
+
+    private void OnRootPathChanged(object sender, TextChangedEventArgs e)
+        => this._tempSettings.Folders.DefaultRoot = this.RootPathBox.Text;
+
+    private void OnPagesPathChanged(object sender, TextChangedEventArgs e)
+        => this._tempSettings.Folders.PagesPath = this.PagesPathBox.Text;
+
+    private void OnComponentsPathChanged(object sender, TextChangedEventArgs e)
+        => this._tempSettings.Folders.ComponentsPath = this.ComponentsPathBox.Text;
+
+    private void OnViewModelsPathChanged(object sender, TextChangedEventArgs e)
+        => this._tempSettings.Folders.ViewModelsPath = this.ViewModelsPathBox.Text;
+
+    private void OnControllersPathChanged(object sender, TextChangedEventArgs e)
+        => this._tempSettings.Folders.ControllersPath = this.ControllersPathBox.Text;
+
+    private void OnApplicationPathChanged(object sender, TextChangedEventArgs e)
+        => this._tempSettings.Folders.ApplicationPath = this.ApplicationPathBox.Text;
+
+    private void OnDtosPathChanged(object sender, TextChangedEventArgs e)
+        => this._tempSettings.Folders.ApplicationDtosPath = this.DtosPathBox.Text;
+
+    private void OnRepositoriesPathChanged(object sender, TextChangedEventArgs e)
+        => this._tempSettings.Folders.RepositoriesPath = this.RepositoriesPathBox.Text;
 
     private void RootPathBox_LostFocus(object sender, RoutedEventArgs e)
     {
